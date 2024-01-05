f73f7c25fe60fbc2c26ef0c52b257f33d84ce37e
etemtolu <42868963+etemtolu@users.noreply.github.com>
2024-01-05 22:43:51 +0300

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,7 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <SearchContextProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>{children}
+        <Analytics />
+        </body>
       </html>
     </SearchContextProvider>
   );
